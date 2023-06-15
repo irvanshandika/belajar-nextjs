8a47e0bdd5844e34b3029099d4c7039ba33576ee
irvanshandika <shandikamuhammadirvan@gmail>
2023-06-15 17:36:36 +0700

    Still try to fix

--- app/components/kontak.jsx
+++ app/components/kontak.jsx
@@ -6,8 +6,56 @@ export default function Contact() {
     <section id="kontak">
       <div className="block max-w-7xl py-28 mx-auto mt-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <div className="flex flex-col items-center pb-10">
-          {/* <h1 className="text-center text-2xl font-bold">Kontak</h1> */}
-          <Kontak />
+          <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+              Contact <span className="text-blue-500">Me</span>
+            </h2>
+            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Jika ada yang ingin ditanyakan? SIlahkan isi pesan dibawah ini :)</p>
+            <form name="messege-to-form" className="form space-y-8">
+              <div>
+                <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Your Name
+                </label>
+                <input
+                  name="nama"
+                  type="text"
+                  id="name"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                  placeholder="Jhon Doe"
+                  required
+                />
+              </div>
+              <div>
+                <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Your Email
+                </label>
+                <input
+                  name="email"
+                  type="email"
+                  id="email"
+                  className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                  placeholder="jhondoe@domain.com"
+                  required
+                />
+              </div>
+              <div className="sm:col-span-2">
+                <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
+                  Your message
+                </label>
+                <textarea
+                  name="pesan"
+                  id="message"
+                  rows="6"
+                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Leave a comment..."></textarea>
+              </div>
+              <button
+                type="submit"
+                className="py-3 px-5 text-sm font-medium text-center text-black rounded-lg bg-blue-400 sm:w-fit hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                Send message
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </section>
