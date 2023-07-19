f21376494b7f8b8d00b1e27845a921d475c6f80f
irvanshandika <shandikamuhammadirvan@gmail>
2023-07-19 23:37:53 +0700

    Change project map in Project

--- app/components/Projek.jsx
+++ app/components/Projek.jsx
@@ -6,59 +6,72 @@ import Projek4 from "./foto4.png";
 import Projek5 from "./foto5.png";
 
 export default function Projek() {
+  const projek = [
+    {
+      title: "First Portfolio HTML, CSS, Javascript",
+      image: Projek1,
+      description: "Projek portfolio pertama dengan menggunakan html, css, javascript, dan bootstrap",
+      alt: "Portfolio HTML, CSS, Javascript",
+      url: "https://irvanshandika.netlify.app",
+    },
+    {
+      title: "May Beauty Skin",
+      image: Projek2,
+      description: "Projek kedua dalam belajar frontend website dengan menggunakan ReactJS dan bootstrap",
+      alt: "May Beauty Skin",
+      url: "https://maybeautyskin.vercel.app",
+    },
+    {
+      title: "Perpustakaan Digital",
+      image: Projek3,
+      description: "Final projek kelompok 6 pada mata kuliah Pemrograman Lanjutan, dengan menggunakan ReactJS dan bootstrap",
+      alt: "Perpustakaan Digital",
+      url: "https://perpustakaandigital.vercel.app/?vercelToolbarCode=Zu-HFLrjhawTWQb",
+    },
+    {
+      title: "Votely",
+      image: Projek4,
+      description: "Final projek Team Votely dengan menggunakan Next JS (Typescript), Tailwindcss, Next Auth, dan Mongodb.",
+      alt: "Votely",
+      url: "https://lets-votely.com",
+    },
+    {
+      title: "Album IF 07",
+      image: Projek5,
+      description: "Projek belajar individu dengan menggunakan Next JS (Typescript), Tailwindcss, dan Cloudinary",
+      alt: "Album foto IF07",
+      url: "https://albumif07.vercel.app",
+    },
+  ];
   return (
     <section id="project" className="z-0">
       <div className="w-full py-28 mx-auto my-4 max-w-7xl bg-white border border-gray-200 rounded-lg shadow ">
-        <h5 className="text-center text-2xl font-bold my-8 font-outfit">Projek <span className="text-sky-500">Saya</span></h5>
+        <h5 className="text-center text-2xl font-bold my-8 font-outfit">
+          Projek <span className="text-sky-500">Saya</span>
+        </h5>
         <p className="text-center text-xl font-bold my-8 font-outfit">Berikut Projek Yang Telah Saya Buat</p>
-        <div className="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[8px] rounded-t-xl h-[172px] max-w-[301px] md:h-[294px] md:max-w-[512px]">
-          <div className="rounded-lg overflow-hidden h-[156px] md:h-[278px] bg-white dark:bg-gray-800">
-            <div id="default-carousel" className="relative w-full z-0" data-carousel="slide">
-              <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <Image src={Projek1} className="block w-full h-auto" alt="Projek 1" />
-                </div>
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <Image src={Projek2} className="block w-full h-auto" alt="Projek 2" />
-                </div>
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <Image src={Projek3} className="block w-full h-auto" alt="Projek 3" />
-                </div>
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <Image src={Projek4} className="block w-full h-auto" alt="Projek 4" />
-                </div>
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <Image src={Projek5} className="block w-full h-auto" alt="Projek 5" />
-                </div>
-              </div>
-              <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-                <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-              </div>
-              <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                  <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {projek.map((item) => (
+            <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                <Image className="rounded-t-lg" src={item.image} alt={item.alt} />
+              </a>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.description}</p>
+                <a
+                  href={item.url}
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Read more
+                  <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                   </svg>
-                  <span className="sr-only">Previous</span>
-                </span>
-              </button>
-              <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                  <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path>
-                  </svg>
-                  <span className="sr-only">Next</span>
-                </span>
-              </button>
+                </a>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="relative mx-auto bg-gray-900 dark:bg-gray-700 rounded-b-xl rounded-t-sm h-[17px] max-w-[351px] md:h-[21px] md:max-w-[597px]">
-          <div className="absolute left-1/2 top-0 -translate-x-1/2 rounded-b-xl w-[56px] h-[5px] md:w-[96px] md:h-[8px] bg-gray-800"></div>
+          ))}
         </div>
       </div>
     </section>
